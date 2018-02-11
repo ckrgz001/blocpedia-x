ce0b1c2b0991175d4d04a0374f85d0cfb32e4339
ckrgz001 <ckrgz001@outlook.com>
2018-02-11 18:15:24 -0500

    more fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,11 +22,5 @@ class User < ApplicationRecord
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
   end
 
-  def gravatar_index(user, options = { size: 80 })
-  gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-  size = options[:size]
-  gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-  image_tag(gravatar_url, alt: user.username)
-end
 
 end

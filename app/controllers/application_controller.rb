86b85457a4927bd281189f42bb9fcf5199fcbb4b
ckrgz001 <ckrgz001@outlook.com>
2018-02-11 19:57:36 -0500

    dont remember

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
     if !users_signed_in?
       flash[:danger] = "You must be logged in to perform that action."
       redirect_to root_path
+    end
+  end
 
 protected
 def configure_permitted_parameters
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    
+  before_action :set_user, only: [:edit, :update, :show]
     
     def index
       @users = User.paginate(:page => params[:page], :per_page => 5)
@@ -13,39 +13,42 @@ class UsersController < ApplicationController
       @user = User.new(user_params)
       if @user.save
         session[:user_id] = @user.id
-        flash[:success] = "Welcome to AlphaBlog, #{@user.username.upcase}!"
+        flash[:success] = "Welcome, #{@user.username.upcase}!"
         redirect_to user_path(@user)
       else
         render 'new' #or :new
       end
     end
       
-      def edit
-      end
+    def edit
+    end
+  
+    def update
+      if @user.update(user_params)
+        flash[:success] = "Info has been updated successfully."
+        redirect_to articles_path
+      else
+        render :edit
+      end 
+    end
     
-      def update
-        if @user.update(user_params)
-          flash[:success] = "Info has been updated successfully."
-          redirect_to articles_path
-        else
-          render :edit
-        end 
-      end
-      
-      def show
-        @user = User.find(params[:id])
-      end
-      
-      def destroy
-        @user = User.find(params[:id])
-        @user.destroy
-        flash[:danger] = "User and all articles by user have been deleted."
-        redirect_to users_path
-      end
+    def show
+    end
+    
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      flash[:danger] = "User and all articles by user have been deleted."
+      redirect_to users_path
+    end
 
        
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end

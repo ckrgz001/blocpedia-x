b74f75807af4b0583e88afed98ae5acfab207f78
ckrgz001 <ckrgz001@outlook.com>
2018-02-12 22:28:10 -0500

    Fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180211010638) do
-=======
 ActiveRecord::Schema.define(version: 20180211193609) do
->>>>>>> User-One-to-Many
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -36,7 +32,6 @@ ActiveRecord::Schema.define(version: 20180211193609) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   create_table "wikis", force: :cascade do |t|

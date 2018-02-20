cfba95087d380cf8c74d2653869d07279a2bbd64
ckrgz001 <ckrgz001@outlook.com>
2018-02-20 17:52:33 -0500

    zeroed out WikiPolicy

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,23 +1,3 @@
 class WikiPolicy < ApplicationPolicy
-    def index?
-      true
-    end
    
-    def create?
-      user.present?
-    end
-   
-    def update?
-      return true if user.present? && user == wiki.user || user.admin?
-    end
-   
-    def destroy?
-      return true if user.present? && user == wiki.user || user.admin?
-    end
-   
-    private
-   
-      def wiki
-        record
-      end
   end

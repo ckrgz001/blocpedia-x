c55b1422f81e0afa2efe7940367cae4d81fa1763
ckrgz001 <ckrgz001@outlook.com>
2018-02-20 06:09:09 -0500

    private

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-15.times do
+15.times do |n|   
+    username = "#{Faker::WorldOfWarcraft.hero}#{n}"        
     User.create!(
-    username: Faker::Kpop.solo,
+    username: username,
     email: Faker::Internet.email,
     password: Faker::Internet.password(10, 20, true, true)
     )
@@ -17,13 +18,24 @@
   
   @users = User.all
   
-  50.times do
+  20.times do
     Wiki.create!(
     title: Faker::Lorem.sentence(3, false, 1),
     body: Faker::Lorem.paragraph(2, true),
-    user: @users.sample
+    user: @users.sample,
+    private: false
     )
   end
   
+  5.times do
+    Wiki.create!(
+    title: Faker::Lorem.sentence(3, false, 1),
+    body: Faker::Lorem.paragraph(2, true),
+    user: @users.sample,
+    private: true
+    )
+  end
+
   puts "Created & Seeded #{User.count} Users to the database."
-  puts "Created & Seeded #{Wiki.count} Wikis to the database."
+  puts "Created & Seeded #{Wiki.count} Public Wikis to the database."
+  puts "Created & Seeded #{Wiki.count} Private Wikis to the database."
